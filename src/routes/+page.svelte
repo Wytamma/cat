8756f2306cb7bdc71c8c0567b4f0285ece550058
Wytamma Wirth <wytamma.wirth@me.com>
2025-01-11 15:38:57 +1100

    :lipstick: better layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,19 +83,34 @@
 			writer.close();
 		}
 	}
+	function resetDropzone() {
+		// get the input element by name 
+		const input = document.getElementsByName('files')[0] as HTMLInputElement;
+		// reset the input element
+		input.value = '';
+		// reset the files
+	}
 </script>
 
-<div class="container h-full w-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
+<div class="h-full flex justify-center items-center mx-2">
+	<div class="max-w-2xl space-y-5 w-full">
 		<div class="flex justify-center items-center space-x-2 align-middle">
-			<span class="text-5xl sm:text-8xl">📄📄📄</span> 
-			<span class="text-3xl sm:text-5xl">➡️</span> 
-			<span class="text-5xl sm:text-8xl">📄</span>
+			<span class="text-6xl sm:text-8xl">📄📄📄</span> 
+			<span class="text-4xl sm:text-5xl">➡️</span> 
+			<span class="text-6xl sm:text-8xl">📄</span>
 		</div>
 		<div class="text-center">
 			<p>Concatenate multiple files into one</p>
 		</div>
-		<FileDropzone name="files" multiple bind:files={files} on:change={concatenateFiles}>
+		<FileDropzone name="files" multiple bind:files={files} on:change={concatenateFiles} on:click={resetDropzone} on:drop={resetDropzone}>
+			<svelte:fragment slot="lead">
+				<div class="flex justify-center">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-16">
+						<path fill-rule="evenodd" d="M5.625 1.5H9a3.75 3.75 0 0 1 3.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 0 1 3.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 0 1-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875Zm6.905 9.97a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72V18a.75.75 0 0 0 1.5 0v-4.19l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z" clip-rule="evenodd" />
+						<path d="M14.25 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 16.5 7.5h-1.875a.375.375 0 0 1-.375-.375V5.25Z" />
+						</svg>	
+				</div>
+			</svelte:fragment>
 			<svelte:fragment slot="message">
 				<p class="text-lg">
 					<span class="font-semibold">Load files</span> or drag and drop
